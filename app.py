0f5aad76592387bc4903f83aaf9ce3c166b15547
DiogoKramel <diogo.kramel@gmail.com>
2019-03-25 04:07:28 +0100

    Initial app boilerplate

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
