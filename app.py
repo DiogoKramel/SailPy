047924b7d83d05669bc7707a1134de6854518643
DiogoKramel <diogo.kramel@gmail.com>
2019-03-25 03:57:45 +0100

    Initial app boilerplate

--- app.py
+++ app.py
@@ -40,5 +40,53 @@ app.layout = html.Div([
     bottombar
 ])
 
+logo = html.Img(src="/assets/static/logoapp.png", height="80px")
+logocapes = html.Img(src="/assets/static/logocapes.png", height="60px")
+logopoli = html.Img(src="/assets/static/logopolitecnica.png", height="60px")
+logousp = html.Img(src="/assets/static/logousp.png", height="60px")
+title = dcc.Link("A preliminary design tool for sailboat optimization", className="navbar-brand")
+
+navitems = html.Ul([
+    dbc.NavItem(dbc.NavLink("Home", href="/introduction")),
+    dbc.DropdownMenu(
+        nav=True, in_navbar=True, label="Menu",
+        children=[
+            dbc.DropdownMenuItem("About"),
+            dbc.DropdownMenuItem(divider=True),
+            dbc.DropdownMenuItem("Authors"),
+            dbc.DropdownMenuItem("Contact"),
+        ],
+    ),
+], className="navbar-nav")
+
+navbar = html.Nav([
+    dbc.Container([
+        dbc.Row([
+            dbc.Col(logo, width="auto"),
+            dbc.Col(title, width="auto"),
+            dbc.Col(navitems, width="auto"),
+        ], justify="between", align="center", style={"width": "100%"}),
+    ]),
+], className="navbar navbar-light navbar-expand-md bg-light sticky-top")
+
+bottombar = html.Nav([
+    dbc.Container([
+        dbc.Row([
+            dbc.Col(logousp, width="auto"),
+            dbc.Col(logopoli, width="auto"),
+            dbc.Col(logocapes, width="auto"),
+        ], align="center", style={"width": "100%"}),
+    ]),
+], className="navbar")
+
+stepbar = dbc.Container([
+    dbc.Nav([
+        dbc.NavItem(dbc.NavLink("Introduction", href="/introduction")),
+        dbc.NavItem(dbc.NavLink("Hull", href="/hull")),
+        dbc.NavItem(dbc.NavLink("Optimization", href="/optimizationhull")),
+        dbc.NavItem(dbc.NavLink("Results I", href="/resultshull")),
+    ], pills=True, justified=True),
+], className="mt-4")
+
 if __name__ == '__main__':
     app.run_server(debug=True)
