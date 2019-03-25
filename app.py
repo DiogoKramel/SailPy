3c7e8bf1aa96c455eda978a137f15bbafe13295b
DiogoKramel <diogo.kramel@gmail.com>
2019-03-25 03:46:54 +0100

    Initial app boilerplate

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
+import os
+
 import dash
-import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-external_stylesheets = [dbc.themes.BOOTSTRAP]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-app.config['suppress_callback_exceptions'] = True
 
-external_css = ["https://use.fontawesome.com/releases/v5.7.2/css/all.css", "assets/fonts/et-line-font/style.css"]
-for css in external_css:
-    app.css.append_css({"external_url": css})
+server = app.server
+
+app.layout = html.Div([
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
+])
+
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
