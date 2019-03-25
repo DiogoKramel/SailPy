78ff0456fc49d0879ec1af6299c9e0bf75fcafd1
DiogoKramel <diogo.kramel@gmail.com>
2019-03-25 04:13:29 +0100

    Initial app boilerplate

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+external_stylesheets = [dbc.themes.BOOTSTRAP]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 server = app.server
+app.config['suppress_callback_exceptions'] = True
+
+external_css = ["https://use.fontawesome.com/releases/v5.7.2/css/all.css", "assets/fonts/et-line-font/style.css"]
+for css in external_css:
+    app.css.append_css({"external_url": css})
+
 
 logo = html.Img(src="/assets/static/logoapp.png", height="80px")
 logocapes = html.Img(src="/assets/static/logocapes.png", height="60px")
