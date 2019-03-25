3b8aa8ae34394c6ea56beeaf748e2b29fafd5b73
DiogoKramel <diogo.kramel@gmail.com>
2019-03-25 04:26:28 +0100

    Initial app boilerplate

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ app.layout = html.Div([
 
 @app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/index' or pathname == '/introduction':
+    if pathname == '/main' or pathname == '/index' orpathname == '/introduction':
         return introduction
     elif pathname == '/hull':
         return hull
